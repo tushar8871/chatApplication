56682f2c7f74c51738fe84eec84c4470e6f1237b
tushar8871 <tusharkhachane6@gmail.com>
2020-04-29 16:49:25 +0530

    Added : chat app

--- Dependency_Scripts/installDependency.sh
+++ Dependency_Scripts/installDependency.sh
@@ -3,10 +3,10 @@ sudo apt-get update
 sudo apt-get install python3-pip python3-dev nginx git -y
 sudo apt-get update
 pip3 install virtualenv
-cd /home/ubuntu/chatApplication
+cd /chatApplication
 virtualenv --python=python3 venv
-source /home/ubuntu/chatApplication/venv/bin/activate
-cd /home/ubuntu/ChatApplication/
+source /chatApplication/venv/bin/activate
+cd /ChatApplication/
 pip3 install -r requirements.txt
 pip3 install django bcrypt django-extensions
 pip3 install gunicorn
@@ -14,12 +14,12 @@ cd fundoo/
 python manage.py collectstatic
 #gunicorn commands
 #gunicorn --bind 0.0.0.0:8000 fundoo.wsgi:application
-sudo cp /home/ubuntu/chatApplication/Dependency_Scripts/gunicorn.service /etc/systemd/system/
+sudo cp /chatApplication/Dependency_Scripts/gunicorn.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start gunicorn
 sudo systemctl enable gunicorn
 #nginx commands
 sudo unlink /etc/nginx/sites-enabled/*
-sudo cp /home/ubuntu/chatApplication/Dependency_Scripts/fundoo /etc/nginx/sites-available/
+sudo cp /chatApplication/Dependency_Scripts/fundoo /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/fundoo /etc/nginx/sites-enabled
 sudo service restart nginx
--- Dependency_Scripts/startApplication.sh
+++ Dependency_Scripts/startApplication.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 sudo service restart gunicorn 
-sudo service status gunicorn 
 sudo service restart nginx
-sudo service status nginx
-
-
